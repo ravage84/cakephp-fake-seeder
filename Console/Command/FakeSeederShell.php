266b84a7b6bf0ced30e69c54b7ece37f7e8d7c57
Marc Würth <ravage@bluewin.ch>
2015-08-31 14:06:02 +0200

    Add a proxy class to use the CakePHP 2.7 shell shortcuts.

---
+++ Console/Command/FakeSeederShell.php
@@ -0,0 +1,10 @@
+<?php
+
+App::uses('SeederShell', 'FakeSeeder.Console/Command');
+
+/**
+ * Fake Seeder Shell
+ *
+ * Just a proxy class to use the CakePHP 2.7 shell shortcuts.
+ */
+class FakeSeederShell extends SeederShell {}
